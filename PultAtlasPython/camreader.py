bdebc25708fd3f9ac723fb115da8a438af480214
incubus-ank <kpahyx@gmail.com>
2021-02-19 11:01:01 +0300

    None

--- PultAtlasPython/camreader.py
+++ PultAtlasPython/camreader.py
@@ -1,21 +1,28 @@
 import numpy as nu
 import cv2
 import time
+import keyboard
 
-cap = cv2.VideoCapture(1)
+def print_pressed_keys(e):
+    print(e.name)
+    e.name = None
 
+
+cap = cv2.VideoCapture(0)
+k = "w"
 
 while(True):
 
     ret, frame = cap.read()
 
-
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+
+    keyboard.hook(print_pressed_keys)
+
+    if cv2.waitKey(1) == ord('q'):  
+            break
 
 cap.release()
 cv2.destroyAllWindow()
